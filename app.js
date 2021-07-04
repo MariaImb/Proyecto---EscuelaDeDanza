00d694d8389a4e711b4c6112e4ae6e602c8a419d
María Imbert <imbert.maria@hotmail.com>
2021-07-03 21:32:44 -0300

    estilos

--- app.js
+++ app.js
@@ -17,3 +17,20 @@ window.onscroll=function()
         }
     }
 
+//NAV ACTIVE LINKS
+const link = document.querySelector('.nav-link');
+    console.log(link);
+
+link.addEventListener('click', function(){
+    link.classList.toggle('is-active');
+    console.log('se hizo click')
+})
+
+
+const menu = document.querySelector('#mobile-menu');
+const menuLinks = document.querySelector ('.nav-menu');
+menu.addEventListener('click', function() {
+    menu.classList.toggle('is-active');
+    menuLinks.classList.toggle('active');
+   
+});
